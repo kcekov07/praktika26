d607b7e8410f97fc3a42a07d9ac0cefe722d6932
kcekov3 <kcekov3@gmail.com>
2026-05-11 21:28:16 +0300

    адд

--- ro-ni-website/js/main.js
+++ ro-ni-website/js/main.js
@@ -1,1 +1,13 @@
-console.log("RO & NI Website Loaded");
+const toggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('nav');
+if (toggle && nav) {
+  toggle.addEventListener('click', () => nav.classList.toggle('open'));
+}
+
+document.querySelectorAll('form').forEach((form) => {
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    alert('Благодарим! Запитването е изпратено успешно.');
+    form.reset();
+  });
+});
